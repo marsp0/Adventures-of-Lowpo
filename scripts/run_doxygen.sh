fb06135ed1a47daebbd163446faadb30368cdde1
marsp0 <martin.n.spasov@gmail.com>
2019-08-18 02:23:10 +0300

    doxygen stuff

--- scripts/run_doxygen.sh
+++ scripts/run_doxygen.sh
@@ -1,5 +1,5 @@
 doxygen -g dconfig
-echo "PROJECT_NAME= Adventures of Lowpo" 	>> dconfig
+echo "PROJECT_NAME=\"Adventures of Lowpo\"" 	>> dconfig
 echo "OUTPUT_DIRECTORY=./docs/" 			>> dconfig
 echo "INLINE_SOURCES=YES" 					>> dconfig
 echo "SOURCE_BROWSER=YES" 					>> dconfig
