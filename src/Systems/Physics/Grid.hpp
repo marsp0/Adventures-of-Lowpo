751ca1b56e3e9d7d3232db6aa917a1532f0026f5
marsp0 <martin.n.spasov@gmail.com>
2019-10-31 20:03:43 +0200

    unit tests update

--- src/Systems/Physics/Grid.hpp
+++ src/Systems/Physics/Grid.hpp
@@ -11,21 +11,21 @@ class Grid
 {
     public:
         
-        /* 
+        /**
         Divides the 3d space into a 2d grid
          */
         Grid(float gridLength, float halfWidth);
-        /* 
+        /** 
         Insert a Collider collider
          */
         void Insert(std::shared_ptr<Collider>   object);
 
-        /* 
+        /** 
         Deletes an object from the grid.
          */
         void Remove(std::shared_ptr<Collider>   object);
         
-        /*
+        /**
         Performs a collision check on all the cells and generates contact data.
          */
         std::vector<std::shared_ptr<Collision>> CheckCollisions();
@@ -36,17 +36,17 @@ class Grid
         // Utility methods
         // ===============
 
-        /* 
+        /** 
         Returns a vector of pairs denoting the cells to be checked for collision detection
         */
         std::vector<std::pair<int, int>> GetEligibleCells(int cellRow, int cellCol);
         
-        /* 
+        /** 
         GetInsertRow returns the row of the cell that the object needs to get inserted into
          */
         int  GetInsertRow(glm::vec3 point);
 
-        /* 
+        /** 
         GetInsertCol returns the column of the cell where the object should be inserted.
          */
         int  GetInsertCol(glm::vec3 point);
